319a349aa8d185b42864604adc8df049e8c11b0f
Roy Conn <rmichael1001@gmail.com>
2023-06-04 10:45:19 -0400

    examples

---
+++ examples/profile.sh
@@ -0,0 +1,197 @@
+#!/usr/bin/env bash
+
+#######################################################################################
+# ███████ ██ ███    ██  █████  ██          ██████  ███████  █████  ██      ███    ███ #
+# ██      ██ ████   ██ ██   ██ ██          ██   ██ ██      ██   ██ ██      ████  ████ #
+# █████   ██ ██ ██  ██ ███████ ██          ██████  █████   ███████ ██      ██ ████ ██ #
+# ██      ██ ██  ██ ██ ██   ██ ██          ██   ██ ██      ██   ██ ██      ██  ██  ██ #
+# ██      ██ ██   ████ ██   ██ ███████     ██   ██ ███████ ██   ██ ███████ ██      ██ #
+#######################################################################################
+# This is a Final Realm profile settings file. DO NOT TOUCH.
+
+# [ User Account ] #
+export USERNAME='admin'
+export PASSWORD='testing'
+export ADMIN=1 # Set to 1 to enable Admin access
+
+# [ Bank ] #
+export bank_1_gold=0
+export bank_1_status='Open'
+export bank_2_gold=0
+export bank_2_status='Open'
+export bank_3_gold=0
+export bank_3_status='Open'
+export bank_4_gold=0
+export bank_4_status='Open'
+export bank_5_gold=0
+export bank_5_status='Open'
+
+# [ Temporary Variables ] #
+export cost=0
+export cost1=0
+export gcho=0
+export echo=0
+export e2cho=0
+export e1=0
+export npc_damage=0
+export ls1=0
+export killcount=0
+export ls2=0
+export la1=0
+export la2=0
+export hitpoints=100
+export original_hitpoints=100
+export armor_type='None'
+export sword_type='None'
+export sword_kind='Hand'
+export buyword1='hi'
+export buyword2='hi'
+export current_lvl=1
+export aan='a'
+export bank_gold=0
+export sword_choice='hi'
+export sword_choice2='hi'
+export sword_choice3='hi'
+export sword_exist='hi'
+export sword_price=0
+export armor_choice='hi'
+export armor_choice2='Armor'
+export armor_price=0
+
+# [ Player Statistics ] #
+export player_xp=0
+export xp_until_next_lvl=500
+export original_xp=500
+export money=100
+export key=0
+export damage=0
+export total_level=13
+
+# [ Woodcutting Skill Stats ] #
+export Woodcutting_lvl=1
+export Woodcutting_xp_target=100
+export Woodcutting_xp_total=38
+
+# [ Cooking Skill Stats ] #
+export Cook_lvl=1
+export Cook_xp_target=100
+export Cook_xp_total=41
+
+# [ Fishing Skill Stats ] #
+export Fishing_lvl=1
+export Fishing_xp_target=100
+export Fishing_xp_total=30
+
+# [ Hunting Skill Stats ] #
+export Hunting_lvl=1
+export Hunting_xp_target=100
+export Hunting_xp_total=30
+
+# [ Mining Skill Stats ] #
+export Mining_lvl=1
+export Mining_xp_target=100
+export Mining_xp_total=35
+
+# [ Smithing Skill Stats ] #
+export Smithing_lvl=1
+export Smithing_xp_target=100
+export Smithing_xp_total=33
+
+# [ Thieving Skill Stats ] #
+export Thieving_lvl=1
+export Thieving_xp_target=100
+export Thieving_xp_total=36
+
+# [ Farming Skill Stats ] #
+export Farming_lvl=1
+export Farming_xp_target=100
+export Farming_xp_total=31
+
+# [ Tailoring Skill Stats ] #
+export Tailoring_lvl=1
+export Tailoring_xp_target=100
+export Tailoring_xp_total=31
+
+# [ Mundane Items Inventory ] #
+export gold_mail=0
+export bone=0
+export dragon_hide=0
+export runic_tablet=0
+export food=0
+export rotten_food=0
+export bait=0
+export potion=0
+export ingot=0
+export seed=0
+export bow=0
+export fur=0
+export gem=0
+export log=0
+export ore=0
+export magical_orb=0
+
+# [ Armor Inventory ] #
+export armor_1=0
+export armor_2=0
+export armor_3=0
+export armor_4=0
+export armor_5=0
+export armor_6=0
+export armor_7=0
+export armor_8=0
+export armor_9=0
+export armor_10=0
+export armor_11=0
+export armor_12=0
+export magic_armor_1=0
+export magic_armor_2=0
+export magic_armor_3=0
+export magic_armor_4=0
+export magic_armor_5=0
+export magic_armor_6=0
+export magic_armor_7=0
+export magic_armor_8=0
+export magic_armor_9=0
+export magic_armor_10=0
+
+# [ Swords Inventory ] #
+export sword_1=0
+export sword_2=0
+export sword_3=0
+export sword_4=0
+export sword_5=0
+export sword_6=0
+export sword_7=0
+export sword_8=0
+export sword_9=0
+export sword_10=0
+export sword_11=0
+export sword_12=0
+export magic_sword_1=0
+export magic_sword_2=0
+export magic_sword_3=0
+export magic_sword_4=0
+export magic_sword_5=0
+export magic_sword_6=0
+export magic_sword_7=0
+export magic_sword_8=0
+export magic_sword_9=0
+export magic_sword_10=0
+
+# [ Pricing ] #
+export price_gold_mail=300
+export price_bone=550
+export price_dragon_hide=750
+export price_runic_tablet=250
+export price_potion=200
+export price_food=100
+export price_ingot=473
+export price_seed=150
+export price_rotten_food=100
+export price_bait=2
+export price_fur=200
+export price_ore=500
+export price_log=275
+export price_gem=1000
+export price_bow=713
+export price_magical_orb=15000
